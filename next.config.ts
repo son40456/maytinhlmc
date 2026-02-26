f30e2369139be841b75ca91f6ab0fdd535a13d0c
ngocsondt <ngocson1102@gmail.com>
2026-02-26 12:56:46 +0700

    chore: allow google and vercel blob image domains

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'maytinhlmc.vn',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+      },
     ],
   },
   async redirects() {
