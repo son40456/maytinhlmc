4ef98b29701712883843f7071e094a75ce2ccde6
ngocsondt <ngocson1102@gmail.com>
2026-03-01 22:56:17 +0700

    Next

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'next.maytinhlmc.vn',
+      },
+      {
+        protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
       {
