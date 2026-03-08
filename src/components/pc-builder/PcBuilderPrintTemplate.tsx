a980c3fa2e3e95aa40dc4b68e5613dab76df66c1
ngocsondt <ngocson1102@gmail.com>
2026-03-08 22:47:13 +0700

    style(pc-builder): further decrease print column widths for price and warranty

--- src/components/pc-builder/PcBuilderPrintTemplate.tsx
+++ src/components/pc-builder/PcBuilderPrintTemplate.tsx
@@ -95,10 +95,10 @@ export const PcBuilderPrintTemplate = forwardRef<HTMLDivElement, PcBuilderPrintT
                             <th className="border border-gray-300 p-2 text-center w-20">Mã SP</th>
                             <th className="border border-gray-300 p-2 text-center w-24">Hình ảnh</th>
                             <th className="border border-gray-300 p-2 text-center">Tên sản phẩm</th>
-                            <th className="border border-gray-300 p-2 text-center w-16">Bảo hành</th>
-                            <th className="border border-gray-300 p-2 text-center w-12">SL</th>
-                            <th className="border border-gray-300 p-2 text-center w-24">Đơn giá</th>
-                            <th className="border border-gray-300 p-2 text-center w-24">Thành tiền</th>
+                            <th className="border border-gray-300 p-2 text-center w-[70px]">Bảo hành</th>
+                            <th className="border border-gray-300 p-2 text-center w-10">SL</th>
+                            <th className="border border-gray-300 p-2 text-center w-[85px]">Đơn giá</th>
+                            <th className="border border-gray-300 p-2 text-center w-[85px]">Thành tiền</th>
                         </tr>
                     </thead>
                     <tbody>
